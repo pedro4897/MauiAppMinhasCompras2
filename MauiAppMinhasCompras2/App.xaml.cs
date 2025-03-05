d62a5316a7b013720f99a14b0c021e9987ec6de2
pedrovitor <pedrovitor@ACER-ASPIRE>
2025-03-05 16:08:35 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras2/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppMinhasCompras2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+    }
+}
