d811f689714bd821fd5bf71d315983c51f6ed2dc
pedrovitor <pedrovitor@ACER-ASPIRE>
2025-03-05 17:16:47 -0300

    atualização da parte da interface integrada ao banco de dados permitindo o salvamento

--- MauiAppMinhasCompras2/App.xaml.cs
+++ MauiAppMinhasCompras2/App.xaml.cs
@@ -1,7 +1,30 @@
-﻿namespace MauiAppMinhasCompras2
+﻿using MauiAppMinhasCompras2.Helpers;
+
+namespace MauiAppMinhasCompras2
+ 
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             //MainPage = new AppShell();
--- MauiAppMinhasCompras2/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras2/Views/NovoProduto.xaml.cs
@@ -1,4 +1,6 @@
 namespace MauiAppMinhasCompras2.Views;
+using MauiAppMinhasCompras2.Models;
+
 
 public partial class NovoProduto : ContentPage
 {
@@ -6,4 +8,29 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+             
+
+
+                
+
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
